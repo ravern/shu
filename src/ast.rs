4453b770d103ea607f135c78677b095ce0dbb757
Ravern Koh <ravernkoh@gmail.com>
2020-12-22 08:25:51 +0000

    remove everything and redo AST

--- src/ast.rs
+++ src/ast.rs
@@ -1,93 +1,143 @@
-use crate::common::ident::Ident;
+use internment::Intern;
 
-pub struct File(pub Vec<Stmt>);
+pub struct Pos {
+  from: usize,
+  to: usize,
+}
+
+pub struct Span {
+  line: Pos,
+  column: Pos,
+}
+
+pub struct Spanned<T> {
+  span: Span,
+  item: T,
+}
+
+impl<T> Spanned<T> {
+  pub fn new(span: Span, item: T) -> Spanned<T> {
+    Spanned { span, item }
+  }
+}
 
 pub enum Stmt {
-  Func {
-    name: Ident,
-    parameters: Vec<Ident>,
-    body: FuncBody,
-    public: bool,
-  },
-  Mod {
-    name: Ident,
-    body: File,
-    public: bool,
-  },
+  Use(UseTree),
+  ModDef(ModDef),
+  FnDef(FnDef),
 }
-
-pub enum FuncBody {
-  Impl {
-    block: Block,
-    template: Option<FuncIdent>,
-  },
-  Template,
-}
-
-pub struct FuncIdent(Vec<Ident>);
-
-pub struct Block(Vec<Expr>);
 
 pub enum Expr {
+  Lit(Lit),
+  Tuple(Vec<Spanned<Expr>>),
+  Record(Vec<(Spanned<Ident>, Spanned<Expr>)>),
+  Label(Label),
+  Assign(Assign),
+  BinOp(BinOp),
+  UnOp(UnOp),
+  If(If),
+  Cond(Cond),
+  Match(Match),
+}
+
+pub struct ModDef {
+  name: Ident,
+  body: Option<ModBody>,
+}
+
+pub struct ModBody {
+  span: Span,
+  stmts: Vec<Stmt>,
+}
+
+pub struct FnDef {
+  name: Ident,
+  body: Option<FnBody>,
+}
+
+pub struct FnBody {
+  exprs: Vec<Expr>,
+  template: Option<Path>,
+}
+
+pub enum Lit {
   Unit,
   Int(i64),
   Float(f64),
-  Bool(bool),
   String(String),
-  Tuple(Vec<Expr>),
-  Record(Vec<(Ident, Expr)>),
-  Closure {
-    // frame: Frame,
-    parameters: Vec<Ident>,
-    body: Block,
-  },
-  Ident(Ident),
-  FuncIdent(FuncIdent),
-  Label(TypeIdent, Box<Expr>),
-  Unary(UnaryOp, Box<Expr>),
-  Binary(BinaryOp, Box<Expr>, Box<Expr>),
-  Assign(Pattern, Box<Expr>),
-  Call(FuncIdent, Vec<Expr>),
-  If(If),
-  Cond(Vec<(Expr, Block)>),
-  Match(Box<Expr>, Vec<(Pattern, Block)>),
+  Bool(bool),
 }
 
-pub struct TypeIdent(Vec<Ident>);
-
-pub enum UnaryOp {
-  Negate,
-  Not,
+pub struct Label {
+  label: Spanned<Path>,
+  arg: Spanned<Box<Expr>>,
 }
 
-pub enum BinaryOp {
+pub struct Assign {
+  lhs: Spanned<Pat>,
+  rhs: Spanned<Box<Expr>>,
+}
+
+pub struct BinOp {
+  kind: BinOpKind,
+  lhs: Spanned<Box<Expr>>,
+  rhs: Spanned<Box<Expr>>,
+}
+
+pub enum BinOpKind {
   Add,
   Sub,
   Mul,
   Div,
-  Mod,
-  Greater,
-  Less,
-  Equal,
-  GreaterEqual,
-  LessEqual,
+  Rem,
   And,
   Or,
-  Pipe,
+  Gt,
+  Gte,
+  Lt,
+  Lte,
+  Eq,
+  Neq,
 }
 
-pub enum Pattern {
-  Label(TypeIdent, Box<Pattern>),
+pub struct UnOp {
+  kind: UnOpKind,
+  arg: Spanned<Box<Expr>>,
+}
+
+pub enum UnOpKind {
+  Neg,
+  Not,
 }
 
 pub struct If {
-  head: Box<Expr>,
+  cond: Spanned<Box<Expr>>,
   body: Block,
-  tail: IfTail,
+  else_: Else,
 }
 
-pub enum IfTail {
+pub enum Else {
+  Empty,
   Else(Block),
-  ElseIf(Box<If>),
-  Done,
+  If(Box<If>),
 }
+
+pub enum Pat {
+  Lit(Lit),
+  Tuple(Vec<Spanned<Pat>>),
+  Record(Vec<(Spanned<Ident>, Spanned<Pat>)>),
+  Label(LabelPat),
+}
+
+pub struct LabelPat {
+  label: Spanned<Path>,
+  arg: Spanned<Box<Pat>>,
+}
+
+pub struct UseTree {}
+
+pub struct Path {
+  idents: Vec<Ident>,
+}
+
+pub struct Ident(Intern<String>);
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
 mod ast;
-mod common;
-mod lex;
-mod parse;
-mod token;
 
 pub fn run() {
-  let source = fallible_iterator::convert::<_, std::io::Error, _>("use test".chars().map(Ok));
-  let tokens = lex::lex(source).unwrap();
-
-  let tokens =
-    fallible_iterator::convert::<_, lex::LexError<std::io::Error>, _>(tokens.into_iter().map(Ok));
-  let file = parse::parse(tokens).unwrap();
+  println!("Hello, world!")
 }
