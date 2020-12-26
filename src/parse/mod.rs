35c14393a8c103737628a4e4cb37353b0e224c73
Ravern Koh <ravernkoh@gmail.com>
2020-12-26 12:32:00 +0800

    refactor `parse` interface

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,8 +1,27 @@
+use std::{fs::File, path::Path};
+
 use crate::{ast::ModBody, common::span::Spanned};
 
-mod lexer;
+pub use self::error::ParseError;
+
+mod error;
+mod lex;
 mod token;
 
-pub fn parse<S>(source: S) -> Result<Spanned<ModBody>, ()> where S: Iterator<Item = char> {
-  Err(())
+pub fn parse_path<P>(path: P) -> Result<Spanned<ModBody>, ParseError>
+where
+  P: AsRef<Path>,
+{
+  Err(ParseError::Syntax(error::SyntaxError {}))
+}
+
+pub fn parse_file(file: File) -> Result<Spanned<ModBody>, ParseError> {
+  Err(ParseError::Syntax(error::SyntaxError {}))
+}
+
+pub fn parse_str<S>(string: S) -> Result<Spanned<ModBody>, ParseError>
+where
+  S: AsRef<str>,
+{
+  Err(ParseError::Syntax(error::SyntaxError {}))
 }
